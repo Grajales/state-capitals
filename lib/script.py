5754565e097b57ce8a7ae161857c106ddf5b69bb
LG29878 <grajalesliliana@johndeere.com>
2021-03-04 22:48:02 -0500

    added trivia game

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,16 @@
 from capitals import states
+import random
+num_states=len(states)
+index=list(range(num_states))
+random.shuffle(index)
+for i in range(0,num_states):
+    picked_state = states[index[i]]["name"]
+    capital=input('What is the capital of '+ picked_state + '?')
+    if (capital == states[index[i]]["capital"]):
+        print("great job!")
+    else:
+        print("keep working at it, try another one")
 
-print(states)
+
+
+# print(states)
